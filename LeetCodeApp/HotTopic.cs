78deaa54c1ff02c047725e7d5534f785c832c6a8
songtaojie <stjworkemail@163.com>
2023-11-28 20:48:06 +0800

    添加最长连续序列

--- LeetCodeApp/HotTopic.cs
+++ LeetCodeApp/HotTopic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,6 +36,41 @@ namespace LeetCodeApp
                 dic[newStr].Add(str);
             }
             return dic.Values.ToList();
+        }
+
+        /// <summary>
+        /// 最长连续序列
+        /// 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度
+        /// 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+        /// </summary>
+        /// <param name="nums">nums = [100,4,200,1,3,2]</param>
+        /// <returns>4,最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。</returns>
+        public static int LongestConsecutive(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            var hashTable = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                if (!hashTable.Contains(num))
+                    hashTable.Add(num);
+            }
+            var result = 0;
+            foreach (int num in hashTable)
+            {
+                if (hashTable.Contains(num - 1))
+                    continue;
+                int currentNum = num;
+                int currentStreak = 1;
+                while (hashTable.Contains(currentNum + 1))
+                {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                result = Math.Max(result, currentStreak);
+            }
+           
+            return result;
         }
     }
 }
--- LeetCodeApp/Program.cs
+++ LeetCodeApp/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCodeApp;
-var result = HotTopic.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+using System.Diagnostics;
+//var result = HotTopic.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+Stopwatch stopwatch = Stopwatch.StartNew();
+stopwatch.Start();
+var result = HotTopic.LongestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 });
+stopwatch.Stop();
+Console.WriteLine($"输出结果为：{result}");
+Console.WriteLine($"耗费时间:{stopwatch.ElapsedMilliseconds}");
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
