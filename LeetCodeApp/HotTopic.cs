6bffb346cc97a8f4b7cd1db1158007fbc27d9bc0
songtaojie <stjworkemail@163.com>
2023-11-30 21:50:24 +0800

    update

--- LeetCodeApp/HotTopic.cs
+++ LeetCodeApp/HotTopic.cs
@@ -72,5 +72,32 @@ namespace LeetCodeApp
            
             return result;
         }
+
+        /// <summary>
+        /// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+        /// 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+        /// 输入: nums = [0,1,0,3,12] 
+        /// 输出: [1,3,12,0,0]
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        public static void MoveZeroes(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return;
+            int fast = 0;
+            int slow = 0;
+            while(fast < nums.Length) 
+            {
+                if (nums[fast] != 0)
+                { 
+                    int tmp = nums[fast]; //            tmp=1
+                    nums[fast] = 0;       //            nums[1] = 0
+                    nums[slow] = tmp;     //            nums[0] = 1
+                    slow++;               //            slow =1
+                }
+                fast++;                   // fast =1    fast = 2
+                                          //[0,1,0,3,12][1,0,0,3,12]
+            }
+        }
     }
 }
